7d3207bf4292a6f80d5bb4a5f2fb632b222912e5
isaac clarke <isaac@ishimura.uk>
2021-03-18 17:49:03 +0000

    placeholder image, profile images loading in wrong? also could probably do with caching results locally then updating each day or something

--- src/index.js
+++ src/index.js
@@ -1,139 +1,146 @@
-console.log("Map of Proof of Humanity Loading...");
+
 import * as axios from 'axios';
 import * as vis from 'vis-network';
 
-async function queryData(url, query){
-    console.log("Querying The Graph...")
-    let data = axios.post(url, {query: query})
-        .then((response)=>{
-            // console.log(response.data);
-            return response.data;
-        })
-        .catch((error)=>{
-            console.log(error);
-            return false;
-        })
-    return data;
-}
+class Diagram {
+    constructor(){
+        console.log("Constructing...");
+        this.element = document.getElementById("diagram");
+        this.graphURL = "https://api.thegraph.com/subgraphs/name/kleros/proof-of-humanity-mainnet";
+        this.graphQuery = "{submissions(where:{registered:true}) {id status registered name vouchees{id} requests{evidence{sender URI}}}}";
+        this.graphData = [];
+        this.structuredData = [];
+        this.ipfs_kleros = "https://ipfs.kleros.io";
+    }
 
-async function buildNodesEdges(data){
-    let nodes = [];
-    let edges = [];
-    console.log("Building Nodes and Edges...")
-    for (var i = 0; i < data.data.submissions.length; i++) {
-        // console.log("Loading: ", i, " of ", data.data.submissions.length-1);
-        
-        let url = data.data.submissions[i].requests[0].evidence[0].URI;
-        // let content = loadContent(url);
-       
-        try{
-            for (var j = 0; j < data.data.submissions[j].vouchees.length; j++) {
-                let edge = {
-                    "from": data.data.submissions[i].id,
-                    "to": data.data.submissions[i].vouchees[j].id
+    async loadGraphData(){
+        console.log("Querying Graph Data...");
+        this.graphData = await axios.post(this.graphURL, {query: this.graphQuery})
+            .then((response)=>{
+                return response.data;
+            })
+            .catch((error)=>{
+                console.log(error);
+                return false;
+            })
+        console.log("GRAPH DATA : ",this.graphData);
+    }
+
+
+    async structureData(){
+        console.log("Structuring Data...");
+        let nodes = [];
+        let edges = [];
+        for (var i = 0; i < this.graphData.data.submissions.length; i++) {
+            try{
+                for (var j = 0; j < this.graphData.data.submissions[j].vouchees.length; j++) {
+                    let edge = {
+                        "from": this.graphData.data.submissions[i].id,
+                        "to": this.graphData.data.submissions[i].vouchees[j].id
+                    }
+                    edges.push(edge);
                 }
-                edges.push(edge);
+            }catch(e){
+                //unregistered vouchee nodes dont exist yet so cant be linked to.
+                // console.log(e);
+            } 
+            let node = {
+                "id": this.graphData.data.submissions[i].id,
+                "label": this.graphData.data.submissions[i].name,
+                "status": this.graphData.data.submissions[i].status,
+                "registered": this.graphData.data.submissions[i].registered,
+                "requests": this.graphData.data.submissions[i].requests,
+                "firstName": "",
+                "lastName": "",
+                "bio": "",
+                "image": "img/placeholder.png",
+                "video": ""
             }
-        }catch(e){
-            //unregistered vouchee nodes dont exist yet so cant be linked to.
-            // console.log(e);
-
-        } 
-
-        let node = {
-            "id": data.data.submissions[i].id,
-            "label": data.data.submissions[i].name,
-            "status": data.data.submissions[i].status,
-            "registered": data.data.submissions[i].registered,
-            "image": "",
-            // "firstName": content.firstName,
-            // "lastName": content.lastName,
-            // "bio": content.bio,
-            // "image": content.image,
-            // "video": content.video
+            nodes.push(node);
         }
-
-        nodes.push(node);
-
-    }
-    return {"nodes":nodes, "edges":edges};
-}
-
-function loadContent(url){
-    let ipfs_kleros = "https://ipfs.kleros.io";
-
-    let content = {
-        firstName: "",
-        lastName: "",
-        bio: "",
-        image: "",
-        video: ""
-    }
-    if(url == undefined || url == null){
-        return content;
+        this.structuredData = {"nodes":nodes, "edges":edges};
+        console.log("STRUCTURED DATA : ", this.structuredData); 
+        return this.structuredData;
     }
 
-    axios.get(ipfs_kleros+url)
-        .then((response)=>{
-            axios.get(ipfs_kleros+response.data.fileURI)
-                .then((response)=>{
-                    content = {
-                        firstName: response.data.firstName,
-                        lastName: response.data.lastName,
-                        bio: response.data.bio,
-                        image: ipfs_kleros+response.data.photo,
-                        video: ipfs_kleros+response.data.video
-                    } 
-                })
-                .catch((error)=>{
-                    console.log("89",error);
-                })
-        })
-        .catch((error)=>{
-            console.log("93",error);
-        })
 
-    return content;
+    async addContent(){
+        console.log("Adding Content...");
+        for(var i = 0; i < this.structuredData.nodes.length; i++) {
+            console.log(this.structuredData.nodes[i]);
+            try{
+                let url = this.structuredData.nodes[i].requests[0].evidence[0].URI;
+                console.log(url);
+                if(url ==  undefined){
+                    break;
+                }
+
+                console.log(this.ipfs_kleros+url);
+                let res = await axios.get(this.ipfs_kleros+url)
+                    .then((response)=>{
+                        axios.get(this.ipfs_kleros+response.data.fileURI)
+                            .then((response)=>{
+                                this.structuredData.nodes[i].firstName = response.data.firstName;
+                                this.structuredData.nodes[i].lastName = response.data.lastName;
+                                this.structuredData.nodes[i].bio = response.data.bio;
+                                this.structuredData.nodes[i].image = this.ipfs_kleros+response.data.photo;
+                                this.structuredData.nodes[i].video = this.ipfs_kleros+response.data.video;
+                            })
+                            .catch((error)=>{
+                                console.log("75",error);
+                            })
+                    })
+                    .catch((error)=>{
+                        console.log("79",error);
+                    })
+
+            }catch(error){
+                console.log(error);
+            }
+        } 
+        console.log("UPDATED STRUCTURED DATA : ",this.structuredData);
+        return this.structuredData;
+    }
+
+    draw(data){
+        console.log("Drawing...");
+        let drawingOptions = {
+            nodes: {
+                shape: "dot",
+                size: 16,
+                // color: "purple",
+                shape: "circularImage"
+            },
+            physics: {
+                forceAtlas2Based: {
+                    gravitationalConstant: -26,
+                    centralGravity: 0.005,
+                    springLength: 230,
+                    springConstant: 0.18,
+                },
+                maxVelocity: 100,
+                solver: "forceAtlas2Based",
+                timestep: 0.15,
+                stabilization: { iterations: 50 },
+            },
+        };
+        let network = new vis.Network(document.getElementById("diagram"), data, drawingOptions);
+        console.log(network);
+    }
 }
 
-function drawDiagram(data){
-    let options = {
-        nodes: {
-          shape: "dot",
-          size: 16,
-          color: "purple",
-          shape: "circularImage"
-        },
-        physics: {
-          forceAtlas2Based: {
-            gravitationalConstant: -26,
-            centralGravity: 0.005,
-            springLength: 230,
-            springConstant: 0.18,
-          },
-          maxVelocity: 100,
-          solver: "forceAtlas2Based",
-          timestep: 0.15,
-          stabilization: { iterations: 50 },
-        },
-      };
-    let network = new vis.Network(document.getElementById("diagram"), data, options);
+//-------------------------------------------------
+async function run(){
+    let diagram = new Diagram();
+    console.log("------------------------------------")
+    let lData = await diagram.loadGraphData();
+    console.log("------------------------------------")
+    let sData = await diagram.structureData();
+    console.log("------------------------------------")
+    // let data = await diagram.addContent()
+    console.log("------------------------------------")
+    diagram.draw(sData);
 }
 
-//---------------------------------------------------------------------------------
-let url = "https://api.thegraph.com/subgraphs/name/kleros/proof-of-humanity-mainnet";
-// 
-// let query = "{submissions(orderBy: creationTime) {id status registered name vouchees{id}}}";
-let query = "{submissions(where:{registered:true}) {id status registered name vouchees{id} requests{evidence{sender URI}}}}";
-let graphData = [];
-let structuredData = [];
-
-async function main() {
-    graphData = await queryData(url, query);
-    console.log("Graph Data:", graphData);
-    structuredData = await buildNodesEdges(graphData);
-    console.log("Structured Data:", structuredData);
-    drawDiagram(structuredData);
-};
-
-main();
+console.log("Map of Proof of Humanity Loading...");
+run();
